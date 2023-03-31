c81b780eccbb03feae7c8d30ce554a42b93f3f0b
Gabriele Simoni <46279871+HotCoffee04@users.noreply.github.com>
2023-03-31 13:57:57 +0200

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
-gcc main.c game.c bcurve.c -static -I"C:\SDL2-2.26.4-mingw\i686-w64-mingw32\include" -L"C:\SDL2-2.26.4-mingw\i686-w64-mingw32\lib" -lSDL2 -lm -lmingw32 -mwindows -Wl,--dynamicbase -ldinput8 -ldxguid -ldxerr8 -luser32 -lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lshell32 -lsetupapi -lversion -luuid -o bcurves.exe
+C:\MinGW\bin\mingw32-gcc main.c game.c bcurve.c -static -I"C:\SDL2-2.26.4-mingw\i686-w64-mingw32\include" -L"C:\SDL2-2.26.4-mingw\i686-w64-mingw32\lib" -lSDL2 -lm -lmingw32 -mwindows -Wl,--dynamicbase -ldinput8 -ldxguid -ldxerr8 -luser32 -lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lshell32 -lsetupapi -lversion -luuid -o bcurves.exe
 pause
 
